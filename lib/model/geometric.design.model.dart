afddee3a64c1144477b204defc65c6320c50b229
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-04 23:40:18 +0100

    create toJson method

--- lib/model/geometric.design.model.dart
+++ lib/model/geometric.design.model.dart
@@ -1,4 +1,6 @@
+import 'package:dart_app/model/circle.model.dart';
 import 'package:dart_app/model/shape.model.dart';
+import 'package:dart_app/model/rectangle.model.dart';
 
 class GeometricDesign {
   List<Shape> shapes=[];
@@ -10,7 +12,23 @@ class GeometricDesign {
 
   void show(){
     for(Shape shape in shapes){
+      if(shape is Circle){
+        print("************ Circle ************");
+        print("Radius : ${shape.getRadius()}");
+      }else if(shape is Rectangle){
+        print("************ Rectangle ************");
+        print("Height : ${shape.getHeight()}");
+        print("Width : ${shape.getWidth()}");
+      }
       print(shape.toJson());
+      print("Area : ${shape.getArea()}");
+      print("Perimeter : ${shape.getPerimeter()}");
     }
+  }
+
+  Map<String,dynamic> toJson(){
+    return {
+      'shapes': shapes
+    };
   }
 }
